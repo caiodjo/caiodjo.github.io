cc884ff424b9178d383322ad6ed214166f520eb9
caiodjo <caiodjo.fernandes@gmail.com>
2022-08-05 02:03:42 -0400

    conclude intro

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,5 @@
-let contato = document.getElementById("contato");
-let sobreMim = document.getElementById("sobre-mim");
-
+let contato = document.getElementById("nav-contato");
+let sobreMim = document.getElementById("nav-sobre-mim");
 
 let borderBottom = function (event) {
     event.target.style.borderBottom = "3px solid black";
@@ -18,3 +17,24 @@ let removeBorder = function (event) {
 
 contato.onmouseover = borderBottom;
 contato.onmouseout = removeBorder;
+
+// Icons flutuantes
+let html = document.getElementById("html");
+let css3 = document.getElementById("css3");
+let js = document.getElementById("js");
+let react = document.getElementById("react");
+
+//Inicializando os icones
+let changePosition = function (event) {
+    const posx = Math.ceil(Math.random()*95);
+    const posy = Math.ceil(Math.random()*95);
+    event.target.style.position = 'absolute';
+    event.target.style.margin = '0';
+    event.target.style.right = `${posx}%`
+    event.target.style.bottom = `${posy}%`
+}
+
+html.onclick = changePosition;
+css3.onclick = changePosition;
+js.onclick = changePosition;
+react.onclick = changePosition;
