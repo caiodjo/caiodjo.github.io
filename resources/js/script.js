2086015448ffa610bc3f02cc34e96e8d6cfa76ba
caiodjo <caiodjo.fernandes@gmail.com>
2022-08-06 14:10:33 -0400

    active/ hover logic done

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,8 +2,6 @@ let sidebar = document.getElementById("mobile-nav");
 let mobile = document.getElementById("menu-mobile");
 let xmark = document.getElementById("xmark");
 
-// let option = document.getElementsByClassName("side-op")[0];
-
 let sideAppear = function (event) {
     sidebar.style.width = "80%";
     sidebar.style.borderLeft = "solid 1px aliceblue";
@@ -17,32 +15,30 @@ let options = ["side-sobre-mim", "side-contato", "side-projetos"];
 let elementosOp = [];
 options.forEach(option => elementosOp.push(document.getElementById(option))); //transforma o array de string em elementos e passa pro array elementosOp
 console.log(elementosOp);
-elementosOp.forEach(element => element.onclick = sideDisappear);
+elementosOp.forEach(element => element.onclick = sideDisappear); //adiciona evento de click em todos os elementos do array
 
 mobile.onclick = sideAppear;
 xmark.onclick = sideDisappear;
-// option.onclick = sideDisappear;
 
-// Icones da intro flutuando loucamente
+// ativa e desativa os links da nav bar
 let contato = document.getElementById("nav-contato");
 let sobreMim = document.getElementById("nav-sobre-mim");
-
+let projetos = document.getElementById("nav-projetos");
+const navElements = [contato, sobreMim, projetos]; //criei a array dos elementos
 let borderBottom = function (event) {
-    event.target.style.borderBottom = "3px solid black";
-    event.target.style.fontWeight = "700";
-    sobreMim.style.borderBottom = "none";
-    sobreMim.style.fontWeight = "500";
+    navElements.forEach(elemento => { //percorro por todos os elementos verificando se ele foi o clicado ou não
+        if(elemento != event.target){ //se nao for o clicado, tiro o active dele e adiciono o hover
+            elemento.classList.remove("active");
+            elemento.classList.add("hover");
+        } else { //se for o clicado adiciono o active e removo o hover
+            elemento.classList.add("active");
+            elemento.classList.remove("hover");
+        }
+    });
 }
-
-let removeBorder = function (event) {
-    event.target.style.borderBottom = "";
-    event.target.style.fontWeight = "";
-    sobreMim.style.borderBottom = "";
-    sobreMim.style.fontWeight = "";
-}
-
-contato.onmouseover = borderBottom;
-contato.onmouseout = removeBorder;
+navElements.forEach(elem =>{
+    elem.onclick = borderBottom;
+})
 
 // Icons flutuantes
 let html = document.getElementById("html");
@@ -50,7 +46,6 @@ let css3 = document.getElementById("css3");
 let js = document.getElementById("js");
 let react = document.getElementById("react");
 
-//Inicializando os icones
 let changePosition = function (event) {
     const posx = Math.ceil(Math.random()*95);
     const posy = Math.ceil(Math.random()*95);
