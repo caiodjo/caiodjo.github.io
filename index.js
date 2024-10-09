b086dacd0b03734ca21ff710ea34d0f8a355f8ea
caiodjo <caiodjo.fernandes@gmail.com>
2024-10-08 22:40:58 -0400

    restore index files

--- index.js
+++
@@ -1,7 +0,0 @@
-var mouse = {
-    nome: "Mouse",
-    preco: 299.99,
-    desconto: 0.2,
-    precoCDesconto: function () { return mouse.preco * mouse.desconto; },
-};
-console.log(mouse.precoCDesconto());
